3f946f97fff76a0434d43cdc5b52b94903d8beb1
johannes gille <johannes.gille@gmx.de>
2023-08-17 16:44:29 +0200

    update filets

--- filet_us.py
+++ filet_us.py
@@ -1,17 +1,16 @@
-[
+f = [
     "kantar > us > health and wellness > health providers and services > no medical check ups in past 5 years",
     "kantar > us > health and wellness > conditions and treatments > rheumatoid arthritis",
     "kantar > us > health and wellness > conditions and treatments > chronic migraine headache",
-        "kantar > us > health and wellness > conditions and treatments > diabetic nerve pain or neuropathy",
-
+    "kantar > us > health and wellness > conditions and treatments > diabetic nerve pain or neuropathy",
     "bk_pain medication buyer_us_third_party_bk_pain medication buyer_us_third_party",
     "kantar > us > health and wellness > outlook > weight loss surgery inclined",
     "health > medical > american academy of urgent care medicine location visitors (dstillery)",
     "kantar > us > health and wellness > telemedicine > parents with children under 18 and would consider using telemedicine",
     "kantar > us > health and wellness > conditions and treatments > blood clots in the legs or dvt",
-    "kantar > us > health and wellness > conditions and treatments > use otc treatment for menstrual cramps or pms"
-    "kantar > us > custom >  use any rx treatment for depression"
-    "kantar > us > health and wellness > conditions and treatments > sleeping difficulty or insomnia and not treating"
+    "kantar > us > health and wellness > conditions and treatments > use otc treatment for menstrual cramps or pms",
+    "kantar > us > custom >  use any rx treatment for depression",
+    "kantar > us > health and wellness > conditions and treatments > sleeping difficulty or insomnia and not treating",
     "sms_new mover > ethnic_minority_group > all african american ethnic groups",
     "visualdna > personality > us > love > passionate lovers",
     "visualdna > personality > us > love > romantic adventurers",
@@ -27,24 +26,18 @@
     "branded data > alc > aristotle political precision (us) > political affiliation by party > republican - hardcore (bluekai)",
     "branded data > alc > aristotle political precision (us) > political affiliation by party > democrat - persuadable/soft (bluekai)",
     "branded data > alc > aristotle political precision (us) > social profiles by type > donald trump supporters (bluekai)",
-    "eyeota - us affinity answers - interest - us politics - advocacy groups and ngos - naral pro-choice america"
-
+    "eyeota - us affinity answers - interest - us politics - advocacy groups and ngos - naral pro-choice america",
     "experian - private > ethnic insight > ethinic group > native american_experian - private > ethnic insight > ethinic group > native american",
     "american student marketing > race > asian/pacific islander",
     "mobilewalla > us > ethno linguistic > asian american",
-    "alikeaudience: united states > demographic > ethnicity > middle eastern"
-
-    "american student marketing > religious affiliation > islam/muslim"
+    "alikeaudience: united states > demographic > ethnicity > middle eastern",
+    "american student marketing > religious affiliation > islam/muslim",
     "american student marketing > religious affiliation > jewish/judaism",
-
-    "branded data > equifax > usa > debt level > high (rt-310735) (retargetly)"
-
+    "branded data > equifax > usa > debt level > high (rt-310735) (retargetly)",
     "adsquare data alliance > us > demographics > age & gender (nielsen dar validated) > male > male 13+ (adsquare)",
     "adsquare data alliance > us > demographics > age & gender (nielsen dar validated) > female > female 13+ (adsquare)",
     "infogroup > consumer > us politics > environment > climate change believer - no",
     "infogroup > consumer > us politics > environment > climate change believer - yes",
-
     "mobilewalla > us > general demography > 13-18",
     "mobilewalla > us > general demography > 19-24",
-
 ]
