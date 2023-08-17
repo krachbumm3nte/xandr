34456e7da28310292ed812e860dc8e53dc63dda0
johannes gille <johannes.gille@gmx.de>
2023-08-17 14:30:16 +0200

    add filets

---
+++ filet_global.py
@@ -0,0 +1,112 @@
+[
+
+    "enlarged prostate (proximic agent)",
+    "zipline prostate disorders"
+    "crossix > crossix ga > diagnosis > atrial fibrillation > 1",
+    "epsilon: healthcare > conditions > heart conditions > irregular heart rhythm",
+    "crossix > crossix ga > diagnosis > congestive heart failure > 2",
+    "crossix wl > health relevance model  > stroke > 1",
+    "skydeo > conditiongraph > disease propensity by type > stroke propensity",
+    "peer39: health>conditions&diseases>cancer",
+    "epsilon: healthcare > conditions > respiratory ailments > asthma",
+    "crossix > crossix wl > diagnosis > leukemia > 3",
+    "disease propensity by type > cancer: skin (adstra)",
+
+    "peer39: custom category>pregnant_12-24 weeks",
+    "peer39: custom category>pregnant_25 plus weeks",
+    "2. pregnant women + ios + female + 21 thru 49 + hhi (2.12.18)_ pregnant mothers feb 2018",
+    "33across > us m life events family planning",
+    "crunchdmc_miscarriages (grapeshot)"
+    "epsilon: healthcare > conditions > menopause",
+    "doubleverify - dv: contextual>health & fitness>infertility",
+    "clickagy > partners > engine group > healthcare/lifestyle > fertility treatment participant/information seekers",
+    "eyeota - us plunge digital - interest - treatment - erectile dysfunction",
+    "sharethis > people & society > disabled & special needs",
+    "nielsen harris poll - employment status - unable to work due to a disability",
+    "crunchdmc_blind_veterans (grapeshot)",
+    "advertise_children_with_autism (grapeshot)",
+    "mediaiq_adhd_medication_kws (grapeshot)",
+    "crossix custom > health relevance model  > adhd recent adult diagnosis (shire) > 1",
+    "prhlthmd_health_panic_anxiety (grapeshot)",
+    "disease propensity by type > anxiety diagnosis (adstra)",
+    "epsilon: healthcare > conditions > depression",
+    "depression propensity - reach tier 3 (adstra)",
+    "clickagy > health > addictions > gambling",
+    "clickagy > health > addictions > drugs",
+    "clickagy > health > addictions > alcohol",
+    "nj_cannabis_af-american_device_cookie",
+    "nj_cannabis_young_voters_v2_device_cookie",
+    "prhlthmd_health_bipolar_disorder (grapeshot)",
+    "prhlthmd_ps_major_depressive_disorder (grapeshot)",
+    "brain and nervous system disorders ( behaviour )",
+    "skydeo > interest > insomnia sufferers > likely",
+    "twine > mobile apps > top mobile apps > grindr - gay chat"
+    "lifestyle > political > lgbt activists - extreme confidence (dstillery)",
+    "social profiles by type > lesbian/gay/bisexual/transgender (lgbt) supporters (adstra)",
+    "delidatax sp - interest - gays and lesbianas (exelate)",
+    "aud.gayfriendly lifestyle_estilo de vida (hd)_aud.gayfriendly lifestyle_estilo de vida (hd) appnexus",
+    "tech targets - from whotoo - industry - memberships and organizations - labor unions (exelate)",
+    "infogroup business and professional - industry - memberships and organizations - labor unions (exelate)",
+    "epsilon: demographics > labor union members > highly likely",
+    "epsilon: books & magazines > bible & devotional readers > highly likely",
+    "33across audienceid - 33across interest segments - food - kosher",
+    "catalina > product attributes >  ingredients  >  lifestyle diet  >  kosher lifestyle"
+
+
+    "l2 voter data > individual demographics > parties description > communist"
+
+
+    "deep root analytics > anti socialism"
+    "very liberal/hard democrat (lotame)",
+    "audience profiles > politics > female democratic supporter (cross pixel)",
+
+    "lifestyle > political > right wing radicals - extreme confidence (dstillery)"
+    "l2 voter data > lifestyle & issue data > haystaqdna > marijuana legalization > support",
+    "lifestyle > activities > marijuana - extreme confidence (dstillery)",
+    "marijuana user: active (adstra)",
+
+    "skydeo > political pinpoints > voter profile > marriage equality supporters",
+    "lifestyle > lifestage > visa & immigration (dstillery)",
+    "lifestyle > lifestage > visa & immigration - extreme confidence (dstillery)",
+
+    "truoptik>webbula>demographics>immigrant assimilation status > partially assimilated (bilingual - english primary)",
+    "truoptik>webbula>demographics>immigrant assimilation status > partially assimilated (bilingual - native language primary)",
+    "truoptik>webbula>demographics>immigrant assimilation status > unassimilated (native language)",
+    "eyeota - au roy morgan - helix personas (2014) - 300 today's families - 305 successful immigrants",
+    "eyeota - nz equifax - landscape - 305 successful immigrants",
+
+    "infogroup > b2c > politics > issues > environment and energy > climate change > believers -- co-op sourced",
+    "infogroup > b2c > politics > issues > environment and energy > climate change > non-believers -- co-op sourced",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
---
+++ filet_us.py
@@ -0,0 +1,50 @@
+[
+    "kantar > us > health and wellness > health providers and services > no medical check ups in past 5 years",
+    "kantar > us > health and wellness > conditions and treatments > rheumatoid arthritis",
+    "kantar > us > health and wellness > conditions and treatments > chronic migraine headache",
+        "kantar > us > health and wellness > conditions and treatments > diabetic nerve pain or neuropathy",
+
+    "bk_pain medication buyer_us_third_party_bk_pain medication buyer_us_third_party",
+    "kantar > us > health and wellness > outlook > weight loss surgery inclined",
+    "health > medical > american academy of urgent care medicine location visitors (dstillery)",
+    "kantar > us > health and wellness > telemedicine > parents with children under 18 and would consider using telemedicine",
+    "kantar > us > health and wellness > conditions and treatments > blood clots in the legs or dvt",
+    "kantar > us > health and wellness > conditions and treatments > use otc treatment for menstrual cramps or pms"
+    "kantar > us > custom >  use any rx treatment for depression"
+    "kantar > us > health and wellness > conditions and treatments > sleeping difficulty or insomnia and not treating"
+    "sms_new mover > ethnic_minority_group > all african american ethnic groups",
+    "visualdna > personality > us > love > passionate lovers",
+    "visualdna > personality > us > love > romantic adventurers",
+    "branded data > alc > aristotle political precision (us) > social profiles by type > likely tea party supporters (bluekai)",
+    "pushspring > custom > miq digital > us - divorced - cross device",
+    "mobilewalla > us > political > conservative",
+    "mobilewalla > us > political > liberal",
+    "branded data > alc > aristotle political precision (us) > 2018's most competitive congressional districts > michigan - 11th congressional district (bluekai)",
+    "branded data > alc > aristotle political precision (us) > voters by voting history > super tuesday voters - democrat (bluekai)",
+    "branded data > alc > aristotle political precision (us) > high dollar donors > high dollar federal donors - republican (bluekai)",
+    "branded data > alc > aristotle political precision (us) > political affiliation by party > split households (bluekai)",
+    "branded data > alc > aristotle political precision (us) > voters by voting history > frequent primary election voters - democrat (bluekai)",
+    "branded data > alc > aristotle political precision (us) > political affiliation by party > republican - hardcore (bluekai)",
+    "branded data > alc > aristotle political precision (us) > political affiliation by party > democrat - persuadable/soft (bluekai)",
+    "branded data > alc > aristotle political precision (us) > social profiles by type > donald trump supporters (bluekai)",
+    "eyeota - us affinity answers - interest - us politics - advocacy groups and ngos - naral pro-choice america"
+
+    "experian - private > ethnic insight > ethinic group > native american_experian - private > ethnic insight > ethinic group > native american",
+    "american student marketing > race > asian/pacific islander",
+    "mobilewalla > us > ethno linguistic > asian american",
+    "alikeaudience: united states > demographic > ethnicity > middle eastern"
+
+    "american student marketing > religious affiliation > islam/muslim"
+    "american student marketing > religious affiliation > jewish/judaism",
+
+    "branded data > equifax > usa > debt level > high (rt-310735) (retargetly)"
+
+    "adsquare data alliance > us > demographics > age & gender (nielsen dar validated) > male > male 13+ (adsquare)",
+    "adsquare data alliance > us > demographics > age & gender (nielsen dar validated) > female > female 13+ (adsquare)",
+    "infogroup > consumer > us politics > environment > climate change believer - no",
+    "infogroup > consumer > us politics > environment > climate change believer - yes",
+
+    "mobilewalla > us > general demography > 13-18",
+    "mobilewalla > us > general demography > 19-24",
+
+]
